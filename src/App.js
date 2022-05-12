a892f7be9635f8c22e6cade84603b704d18d6482
Andrea Martin Rodriguez <amartin.onda@gmail.com>
2022-05-12 08:14:06 +0200

    Added todoForm funtion to create todo items

--- src/App.js
+++ src/App.js
@@ -9,6 +9,24 @@ function Todo({todo}){
   )
 }
 
+function TodoForm({addTodo}){
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className= "input" value={value} onChange={e => setValue(e.target.value)}>    
+      </input>
+    </form>
+  )
+}
+
 function App() {
 
   const[todos, setTodos] = React.useState([
